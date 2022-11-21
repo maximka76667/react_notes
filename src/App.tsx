bc1201d993909f2200f6921e4f283a652c6b8a70
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-11-21 18:41:52 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi</h1>
+    </div>
+  );
+}
+
+export default App;
