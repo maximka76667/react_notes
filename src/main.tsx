96b83ac4088bf5e829a48ae6f4cbe73d7cc8b7a4
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-11-25 21:47:41 +0100

    fix basename

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react_notes/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
